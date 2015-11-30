ba2fc14f9c2b681fb54fc6ee9e7902b58c7531c5
Malian Zikri <malianzikri92@gmail.com>
2015-11-30 13:20:54 +0700

    Add Project Generator
    Add project generator xml yang berupa isi dari 2 class. yaitu class
    utama dan class generatexml

---
+++ src/Main/Generator.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author Malian
+ * Class ini digunakan untuk memanggil class-class lain berupa generator file
+ */
+public class Generator {
+    public String generatorXML(String tagging, String isi)
+    {
+        String xml = null;
+        /*
+        proses memanggil class lain yang berupa class generator xml
+        */
+        return xml;
+    }
+}
