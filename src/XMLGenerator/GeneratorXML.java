79930592aad17aa46f374de48938cc90ab3abbb0
Kamal Bakri <k.241293@gmail.com>
2015-12-01 15:41:40 +0700

    add 4 method
    method
    getString(),saveXml(),set_attribute_header(),set_attribute_child()

--- src/XMLGenerator/GeneratorXML.java
+++ src/XMLGenerator/GeneratorXML.java
@@ -5,6 +5,8 @@
  */
 package XMLGenerator;
 
+import java.io.FileWriter;
+import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.output.Format;
@@ -12,12 +14,11 @@ import org.jdom2.output.XMLOutputter;
 
 /**
  *
- * @author Malian 
- * class yang berfungsi memanggil metode atau function class ini
+ * @author Malian class yang berfungsi memanggil metode atau function class ini
  * akan memanggil metode atau function yang dibutuhkan untuk menggenerate xml
- * kode 0: untuk memasukan data langsung ke header
- * kode 1: untuk memasukan data langsung ke child yang berada dibawah header
- * kode 2: untuk memasukan data langsung ke child yang berada didalam child
+ * kode 0: untuk memasukan data langsung ke header kode 1: untuk memasukan data
+ * langsung ke child yang berada dibawah header kode 2: untuk memasukan data
+ * langsung ke child yang berada didalam child
  */
 public class GeneratorXML {
 
@@ -41,8 +42,15 @@ public class GeneratorXML {
             String split_element[] = tagging_split[i].split(":");
             header[index_header].addContent(new Element(split_element[0].trim()).setText(split_element[1].trim()));
         }
-
 //        doclist.addContent(header[index_header]);
+    }
+
+    public void set_attribute_header(String nama_attrib, String isi_attrib, int index_header) {
+        header[index_header].setAttribute(new Attribute(nama_attrib, isi_attrib));
+    }
+
+    public void set_attribute_child(String nama_attrib, String isi_attrib, int index_child) {
+        child[index_child].setAttribute(new Attribute(nama_attrib, isi_attrib));
     }
 
     public void set_tag_child(String tag_child) {
@@ -75,13 +83,27 @@ public class GeneratorXML {
             child[index_header].addContent(child[index_child]);
         }
     }
-    public void final_generator()
-    {
-         doc.getRootElement().addContent(doclist);
+
+    public String getStringxml() {
+        doc.getRootElement().addContent(doclist);
         String xml = new XMLOutputter().outputString(doc);
-        XMLOutputter xmlOutput = new XMLOutputter();
-        xmlOutput.setFormat(Format.getPrettyFormat());
-        System.out.println(xml);
+        return xml;
+    }
+
+    public Boolean saveXml(String path, String nama_file) {
+        try {
+            doc.getRootElement().addContent(doclist);
+            XMLOutputter xmlOutput = new XMLOutputter();
+            xmlOutput.setFormat(Format.getPrettyFormat());
+            xmlOutput.output(doc, new FileWriter(path + nama_file+".xml"));
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
+    public void final_generator() {
+        
     }
 
 }
