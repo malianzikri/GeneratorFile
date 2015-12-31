303dc091064512a8f8ed4b383603edca1693bca1
Kamal Bakri <k.241293@gmail.com>
2015-12-31 15:43:36 +0700

    update parsing xml
    update parsing xml

--- src/XMLGenerator/ParsingXML.java
+++ src/XMLGenerator/ParsingXML.java
@@ -4,10 +4,9 @@
  * and open the template in the editor.
  */
 package XMLGenerator;
-
-import File.TXT.CreateFile;
 import java.io.IOException;
 import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -22,84 +21,42 @@ import org.jdom2.input.SAXBuilder;
  */
 public class ParsingXML {
 
-    Element elemen[] = null;
+    List list[] = null;
+    List<Element> ele = new ArrayList<>();
+    ArrayList al = new ArrayList();
 
-    private String xmlParsing(String xml, String root, String tagging) {
-        String[] header;
-        List listchild = null;
-        Element elemen[] = null;
-        List list = null;
+    private ArrayList xmlParsing(String xml, String root, String nodes) {
         String result = "";
-
         xml = xml.replace("xmlns", "doc");
         xml = xml.replace("xmlns=\"\"", "");
         xml = xml.replaceAll(" /", "/");
-
         SAXBuilder builder = new SAXBuilder();
-
         try {
             Document documentparse = builder.build(new StringReader(xml));
-            int index = 0;
-            header = root.split(">");
-            int jml_header = header.length;
-            elemen = new Element[jml_header];
-            for (String head : header) {
-                if (index == 0) {
-                    elemen[index] = documentparse.getRootElement();
-                    list = elemen[index].getChildren(head);
-                    jml_header--;
-                    if (jml_header == 0) {
-                        for (int j = 0; j < list.size(); j++) {
-                            Element node = (Element) list.get(j);
-                            String[] split_tagging = tagging.split(",");
-                            int cek = 1;
-                            for (String tag : split_tagging) {
-                                result += node.getChildText(tag);
-                                result += (cek == split_tagging.length) ? "" : ",";
-                                cek++;
-                            }
-                            result += ";";
-                        }
+            String[] split_root = root.split(">");
+            int jml_root = split_root.length;
+            list = new List[jml_root];
+            ele = new ArrayList();
+            al = new ArrayList();
+            ele.add(documentparse.getRootElement());
+            list[ele.size() - 1] = ele.get(0).getChildren(split_root[0]);
+            int index = jml_root;
+
+            for (int x = 1; x <= jml_root; x++) {
+                if (index == 1) {
+                    for (int j = 0; j < list[ele.size() - 1].size(); j++) {
+                        Element node = (Element) list[ele.size() - 1].get(j);
+                        result += node.getChildText(nodes);
+                        result += (j == list[x - 1].size() - 1) ? "" : ",";
                     }
+                    al.add(result);
                 } else {
-                    jml_header--;
-                    if (list.size() == 1) {
-                        elemen[index] = (Element) list.get(0);
-                        list = elemen[index].getChildren(head);
-                        if (jml_header == 0) {
-                            for (int j = 0; j < list.size(); j++) {
-                                Element node = (Element) list.get(j);
-                                String[] split_tagging = tagging.split(",");
-                                int cek = 1;
-                                for (String tag : split_tagging) {
-                                    result += node.getChildText(tag);
-                                    result += (cek == split_tagging.length) ? "" : ",";
-                                    cek++;
-                                }
-                                result += ";";
-                            }
-                        }
-                    } else {
-                        for (int i = 0; i < list.size(); i++) {
-                            elemen[index] = (Element) list.get(i);
-                            listchild = elemen[index].getChildren(head);
-                            if (jml_header == 0) {
-                                for (int j = 0; j < listchild.size(); j++) {
-                                    Element node = (Element) listchild.get(j);
-                                    String[] split_tagging = tagging.split(",");
-                                    int cek = 1;
-                                    for (String tag : split_tagging) {
-                                        result += node.getChildText(tag);
-                                        result += (cek == split_tagging.length) ? "" : ",";
-                                        cek++;
-                                    }
-                                    result += ";";
-                                }
-                            }
-                        }
+                    for (int j = 0; j < list[(x - 1)].size(); j++) {
+                        ele.add((Element) list[(x - 1)].get(j));
+                        list[ele.size() - 1] = ele.get(ele.size() - 1).getChildren(split_root[x]);
+                        index--;
                     }
                 }
-                index++;
             }
 
         } catch (JDOMException ex) {
@@ -107,47 +64,46 @@ public class ParsingXML {
         } catch (IOException ex) {
             Logger.getLogger(ParsingXML.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return result;
+        return al;
     }
 
-    public String getStringParsingXml(String xml, String root, String tagging) {
-        String res = null;
-        ParsingXML xp = new ParsingXML();
-        res = xp.xmlParsing(xml, root, tagging);
-        if (!res.equals("")) {
-            return res;
-        } else {
-            return "datakosong";
+    public ArrayList getHeader(String fStream, String typedok) {
+        ArrayList<String> doks = new ArrayList<String>();
+        int i = 0;
+        String data = fStream;
+        String[] databaru = data.replace("</"+typedok+">", "</"+typedok+">" + "#").split("#");
+        for (String dok : databaru) {
+            i++;
+            if (i == databaru.length) {
+                dok = "";
+            } else if (!dok.contains("<DOCUMENT>")) {
+
+                doks.add("<DOCUMENT>" + dok + "</DOCUMENT>");
+            } else {
+                doks.add(dok + "</DOCUMENT>");
+            }
         }
-    }
-
-    public boolean CreateFileXml(String path, String namafile, String xml, String root, String tagging) {
-        String isi = null;
-        Boolean result;
-
-        ParsingXML xp = new ParsingXML();
-        isi = xp.xmlParsing(xml, root, tagging);
-        CreateFile cf = new CreateFile();
-        result = cf.newFileParsing(path, namafile, isi);
-
-        return result;
+        return doks;
     }
 
     public static void main(String[] args) {
-        // TODO code application logic here
+//        // TODO code application logic here
         String xmla = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-                    + "<DOCUMENT>"
-                    + "<LOADPLP><HEADER><KD_DOK>1</KD_DOK></HEADER><HEADER><KD_DOK>2</KD_DOK></HEADER><DETIL><CONT><NO_CONT>4</NO_CONT></CONT></DETIL></LOADPLP>"
-                    + "<LOADPLP><HEADER><KD_DOK>5</KD_DOK></HEADER><DETIL><CONT><NO_CONT>4</NO_CONT></CONT></DETIL></LOADPLP>"
-                    + "</DOCUMENT>";
+                + "<DOCUMENT>"
+                + "<LOADPLP><HEADER><KD_DOK>1</KD_DOK></HEADER><DETIL><CONT><NO_CONT>4</NO_CONT></CONT><CONT><NO_CONT>5</NO_CONT></CONT></DETIL></LOADPLP>"
+                + "<LOADPLP><HEADER><KD_DOK>9</KD_DOK></HEADER><DETIL><CONT><NO_CONT>3</NO_CONT></CONT></DETIL></LOADPLP>"
+                + "<LOADPLP><HEADER><KD_DOK>5</KD_DOK></HEADER><HEADER><KD_DOK>2</KD_DOK></HEADER><DETIL><CONT><NO_CONT>6</NO_CONT></CONT><CONT><NO_CONT>8</NO_CONT></CONT></DETIL></LOADPLP>"
+                + "</DOCUMENT>";
         ParsingXML a = new ParsingXML();
-//        String hasil = a.getStringParsingXml(xmla, "LOADPLP>HEADER", "KD_DOK");
-        String hasil = a.getStringParsingXml(xmla, "LOADPLP", "");
-        String[] hasil1 = hasil.split(";");
-        for(int i=0;i<hasil1.length;i++){
-            String hasil2 = a.getStringParsingXml(xmla, "LOADPLP>HEADER", "KD_DOK");
-            System.out.println(hasil2);
+        ArrayList<String> head = new ArrayList();
+        ArrayList<String> node = new ArrayList();
+        head = a.getHeader(xmla, "LOADPLP");
+        for (String head1 : head) {
+            node = a.xmlParsing(head1, "LOADPLP>DETIL>CONT", "NO_CONT");
+            for(String nodes : node){
+                System.out.println(nodes);
+            }
         }
-        
     }
 }
+
